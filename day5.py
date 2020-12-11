0d0b1694552e42d3476ad6c2cfe30e9a6aa3d9b3
Vivian <vivianvanoijen@protonmail.com>
2020-12-11 23:07:28 +0100

    Complete first puzzle of day 5

---
+++ day5.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import math
+
+file_name = "input.txt"
+
+if not os.path.isfile(file_name):
+    raise ValueError("Given input file cannot be found.")
+
+file = open(file_name, "r")
+contents = file.readlines()
+file.close()
+
+highest_id = 0
+
+for line in contents:
+    row_indication = line[:7]
+    col_indication = line[7:]
+
+    min_row = 0
+    max_row = 127
+    for letter in row_indication:
+        difference = max_row - min_row
+        if letter == "F":
+            max_row = math.floor(max_row - (difference/2))
+        if letter == "B":
+            min_row = math.ceil(min_row + (difference/2))
+    row = max_row
+
+    min_col = 0
+    max_col = 7
+    for letter in col_indication:
+        difference = max_col - min_col
+        if letter == "L":
+            max_col = math.floor(max_col - (difference/2))
+        if letter == "R":
+            min_col = math.ceil(min_col + (difference/2))
+    col = max_col
+
+    seat_id = row*8 + col
+
+    if seat_id > highest_id:
+        highest_id = seat_id
+
+print(highest_id)
